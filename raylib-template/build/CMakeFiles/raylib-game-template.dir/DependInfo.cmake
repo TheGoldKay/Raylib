3851efce45fff3c729891943187a015464b1efa2
GoldKay <jhonnatanwfc@live.com>
2024-10-17 08:52:32 -0300

    Initial Commit

---
+++ raylib-template/build/CMakeFiles/raylib-game-template.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/goldkay/Code/Raylib/raylib-template/src/raylib_game.c" "CMakeFiles/raylib-game-template.dir/src/raylib_game.c.o" "gcc" "CMakeFiles/raylib-game-template.dir/src/raylib_game.c.o.d"
+  "/home/goldkay/Code/Raylib/raylib-template/src/screen_ending.c" "CMakeFiles/raylib-game-template.dir/src/screen_ending.c.o" "gcc" "CMakeFiles/raylib-game-template.dir/src/screen_ending.c.o.d"
+  "/home/goldkay/Code/Raylib/raylib-template/src/screen_gameplay.c" "CMakeFiles/raylib-game-template.dir/src/screen_gameplay.c.o" "gcc" "CMakeFiles/raylib-game-template.dir/src/screen_gameplay.c.o.d"
+  "/home/goldkay/Code/Raylib/raylib-template/src/screen_logo.c" "CMakeFiles/raylib-game-template.dir/src/screen_logo.c.o" "gcc" "CMakeFiles/raylib-game-template.dir/src/screen_logo.c.o.d"
+  "/home/goldkay/Code/Raylib/raylib-template/src/screen_options.c" "CMakeFiles/raylib-game-template.dir/src/screen_options.c.o" "gcc" "CMakeFiles/raylib-game-template.dir/src/screen_options.c.o.d"
+  "/home/goldkay/Code/Raylib/raylib-template/src/screen_title.c" "CMakeFiles/raylib-game-template.dir/src/screen_title.c.o" "gcc" "CMakeFiles/raylib-game-template.dir/src/screen_title.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/goldkay/Code/Raylib/raylib-template/build/_deps/raylib-build/raylib/CMakeFiles/raylib.dir/DependInfo.cmake"
+  "/home/goldkay/Code/Raylib/raylib-template/build/_deps/raylib-build/raylib/external/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ raylib-template/build/CMakeFiles/raylib-game-template.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raylib-game-template.dir/src/raylib_game.c.o"
+  "CMakeFiles/raylib-game-template.dir/src/raylib_game.c.o.d"
+  "CMakeFiles/raylib-game-template.dir/src/screen_ending.c.o"
+  "CMakeFiles/raylib-game-template.dir/src/screen_ending.c.o.d"
+  "CMakeFiles/raylib-game-template.dir/src/screen_gameplay.c.o"
+  "CMakeFiles/raylib-game-template.dir/src/screen_gameplay.c.o.d"
+  "CMakeFiles/raylib-game-template.dir/src/screen_logo.c.o"
+  "CMakeFiles/raylib-game-template.dir/src/screen_logo.c.o.d"
+  "CMakeFiles/raylib-game-template.dir/src/screen_options.c.o"
+  "CMakeFiles/raylib-game-template.dir/src/screen_options.c.o.d"
+  "CMakeFiles/raylib-game-template.dir/src/screen_title.c.o"
+  "CMakeFiles/raylib-game-template.dir/src/screen_title.c.o.d"
+  "raylib-game-template/raylib-game-template"
+  "raylib-game-template/raylib-game-template.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/raylib-game-template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
